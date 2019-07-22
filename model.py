affed5616efe42b26d080d288a141c623867f737
Jaskaran Singh <32906806+Jaskaran170599@users.noreply.github.com>
2019-07-22 13:34:25 +0530

    trainging

--- model.py
+++ model.py
@@ -1,5 +1,5 @@
 import keras as ks
-# import tensorflow as tf
+
 
 def create_model(input_shape):
     base_model=ks.applications.VGG16(include_top=False,input_shape=input_shape)
@@ -13,13 +13,14 @@ def create_model(input_shape):
     model=ks.models.Sequential()
     model.add(base_model)
     model.add(ks.layers.Flatten())
-    model.add(ks.layers.Dropout(rate=0.25))
+    model.add(ks.layers.Dropout(rate=0.5))
     model.add(ks.layers.Dense(1024,activation="relu"))
     model.add(ks.layers.Dense(512,activation="relu"))
     model.add(ks.layers.Dense(128,activation="relu"))
+    model.add(ks.layers.Dropout(rate=0.5))
     model.add(ks.layers.Dense(64,activation="relu"))
     model.add(ks.layers.Dense(1,activation="sigmoid"))
-
-    model.compile(optimizer="adam",loss=ks.losses.binary_crossentropy,metrics=["accuracy"])
+    opti=ks.optimizers.adamax(lr=0.0007)
+    model.compile(optimizer=opti,loss=ks.losses.binary_crossentropy,metrics=["accuracy"])
 
     return model
